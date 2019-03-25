f2e754b39723c39c65a7ee25bd282c564a856f9f
Sus Verwimp <sus.verwimp@hotmail.com>
2019-03-25 11:11:08 +0100

    Docker structure changes and omopcdm implementation with shared folder

---
+++ secure/start-omopcdm-indexes-and-constraints.cmd
@@ -0,0 +1,46 @@
+@echo off
+
+echo Docker login, Make sure to use an account with access to the honeur docker hub images.
+docker login
+
+IF %ERRORLEVEL% EQU 0 (
+    goto honeur_setup
+) else (
+    echo Failed to Login
+	goto eof
+)
+
+:honeur_setup
+echo Press [Enter] to start removing the existing containers
+pause>NUL
+
+echo Stop previous containers. Ignore errors when no omop-indexes-and-constraints container exist yet.
+echo stop omop-indexes-and-constraints
+docker stop omop-indexes-and-constraints
+
+echo Removing previous containers. This can give errors when no omop-indexes-and-constraints container exist yet.
+echo remove omop-indexes-and-constraints
+docker rm omop-indexes-and-constraints
+
+echo Succes
+echo Press [Enter] key to continue
+pause>NUL
+
+echo Creating folder setup-conf
+mkdir setup-conf
+echo Downloading docker-compose.yml file.
+curl -L https://raw.githubusercontent.com/solventrix/Honeur-Setup/master/secure/OMOPCDMDBIndexesAndContraints/docker-compose.yml --output docker-compose.yml
+echo Downloading setup.yml file inside setup-conf folder
+curl -L https://raw.githubusercontent.com/solventrix/Honeur-Setup/master/secure/OMOPCDMDBIndexesAndContraints/setup-conf/setup.yml --output setup-conf/setup.yml
+
+docker-compose rm -f
+docker-compose pull
+docker-compose up
+
+echo success
+goto eof
+
+
+:eof
+echo Press [Enter] key to exit
+pause>NUL
