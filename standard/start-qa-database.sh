335153d4ee64a80c849ed4d35910c2119a9eb973
Sus Verwimp <sus.verwimp@hotmail.com>
2019-03-26 13:10:06 +0100

    qa database script unix support

--- standard/start-qa-database.sh
+++ standard/start-qa-database.sh
@@ -7,29 +7,34 @@ if [ $? -eq 0 ]
 then
     read -p "Press [Enter] to start removing the existing containers"
 
-    echo Stop previous containers. Ignore errors when no webapi-source-qa-enable container exist yet.
+    echo Stop previous containers. Ignore errors when no containers exist yet.
+    echo stop postgres-qa
+    docker stop postgres-qa
     echo stop webapi-source-qa-enable
     docker stop webapi-source-qa-enable
     
-    echo Removing previous containers. This can give errors when no webapi-source-qa-enable container exist yet.
+    echo Removing previous containers. This can give errors when no containers exist yet.
+    echo remove postgres-qa
+    docker rm postgres-qa
     echo remove webapi-source-qa-enable
     docker rm webapi-source-qa-enable
     
     echo Succes
     read -p "Press [Enter] key to continue"
 
-    echo Creating folder setup-conf
     mkdir setup-conf
     echo Downloading docker-compose.yml file.
     curl -L https://raw.githubusercontent.com/solventrix/Honeur-Setup/master/standard/WebAPIDBQASourceCreation/docker-compose.yml --output docker-compose.yml
     echo Downloading setup.yml file inside setup-conf folder
     curl -L https://raw.githubusercontent.com/solventrix/Honeur-Setup/master/standard/WebAPIDBQASourceCreation/setup-conf/setup.yml --output setup-conf/setup.yml
+    
+    docker volume create --name pgdata-qa
+    docker volume create --name shared-qa
 
     docker-compose pull
-    docker-compose up
+    docker-compose up -d
     
-    echo success
-
+    echo postgresql is available on localhost:5445
 fi
 read -p "Press [Enter] key to exit"
 echo bye
