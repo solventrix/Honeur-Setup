c3f97b3028bcfa560d29292154d9d7977d786fb7
petermoorthamer <moorthamer@gmail.com>
2019-05-07 10:49:08 +0200

    Build a user friendly solution to run the ETL at the remote site (JIRA AEKR-621)

---
+++ RunETLCMG/runETL.sh
@@ -0,0 +1,18 @@
+docker stop run-python-etl
+docker rm run-python-etl
+
+rm -rf ./CMG
+
+git clone https://github.com/solventrix/CMG ./CMG
+
+curl -L https://raw.githubusercontent.com/solventrix/Honeur-Setup/master/RunETLCMG/docker-compose.yml --output docker-compose.yml
+
+read -p "DB username [honeur_admin]: " db_username
+db_username=${db_username:-honeur_admin}
+read -p "DB password [honeur_admin]: " db_password
+db_password=${db_password:-honeur_admin}
+
+sed -i -e "s/db_username/$db_username/g" docker-compose.yml
+sed -i -e "s/db_password/$db_password/g" docker-compose.yml
+
+docker-compose up
