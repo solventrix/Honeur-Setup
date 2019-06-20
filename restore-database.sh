f639674037543f469af04702a2e44563b2deea4f
petermoorthamer <moorthamer@gmail.com>
2019-06-20 18:22:13 +0200

    Create script to backup and a script to restore the data at the remote sites (JIRA AEKR-641)

---
+++ restore-database.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+volume="pgdata"
+
+if [ "$1" = "" ]
+then
+        echo "Please provide the backup file to restore from"
+        exit
+fi
+
+if [ "$2" != "" ]
+then
+        volume="$2"
+fi
+
+docker run -v ${volume}:/volume -v $PWD:/backup --rm loomchild/volume-backup restore $1
