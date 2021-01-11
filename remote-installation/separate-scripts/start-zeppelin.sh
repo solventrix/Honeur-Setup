a3c484ad4b311a001632fc9bfd637d3280ff315c
Sus Verwimp <sus.verwimp@hotmail.com>
2021-01-11 19:07:34 +0100

    remove read-only

--- remote-installation/separate-scripts/start-zeppelin.sh
+++ remote-installation/separate-scripts/start-zeppelin.sh
@@ -72,10 +72,8 @@ docker run \
 -v $HONEUR_ZEPPELIN_NOTEBOOKS:/notebook \
 -m "1.5g" \
 --cpus "1" \
---read-only \
 --pids-limit 100 \
 --cpu-shares 1024 \
---tmpfs /tmp \
 --ulimit nofile=1024:1024 \
 -d \
 honeur/zeppelin:$TAG > /dev/null 2>&1
