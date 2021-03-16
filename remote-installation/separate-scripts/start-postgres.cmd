2ebf244424e66e86d4218091613637cef03110c4
Sus Verwimp <sus.verwimp@hotmail.com>
2021-03-16 17:31:37 +0100

    BUG fix random generator for windows

--- remote-installation/separate-scripts/start-postgres.cmd
+++ remote-installation/separate-scripts/start-postgres.cmd
@@ -103,3 +103,11 @@ SET _tmp=%_Str:~9,1%
 SET /A _Len=_Len+_tmp
 Set _count=0
 SET _RndAlphaNum=
+:_loop
+Set /a _count+=1
+SET _RND=%Random%
+Set /A _RND=_RND%%%_Len%
+SET _RndAlphaNum=!_RndAlphaNum!!_Alphanumeric:~%_RND%,1!
+If !_count! lss %_RNDLength% goto _loop
+ENDLOCAL & SET %~1=%_RndAlphaNum%
+EXIT /B 0
