b2fe25cf2dca0ed8be199a461a31e5631da3157b
Sus Verwimp <sus.verwimp@hotmail.com>
2021-08-02 11:48:03 +0200

    use honeur harbor as default public repo for installation script

--- remote-installation/separate-scripts/start-user-management.sh
+++ remote-installation/separate-scripts/start-user-management.sh
@@ -1,2 +1,2 @@
 docker network create feder8-net >/dev/null 2>&1 || true
-docker run --rm -it --network feder8-net -v /var/run/docker.sock:/var/run/docker.sock feder8/install-script:2.0.0 feder8 init user-management
+docker run --rm -it --network feder8-net -v /var/run/docker.sock:/var/run/docker.sock harbor.honeur.org/library/install-script:2.0.0 feder8 init user-management
--- remote-installation/separate-scripts/start-zeppelin.sh
+++ remote-installation/separate-scripts/start-zeppelin.sh
@@ -1,2 +1,2 @@
 docker network create feder8-net >/dev/null 2>&1 || true
-docker run --rm -it --network feder8-net -e CURRENT_DIRECTORY=$(pwd) -e IS_WINDOWS=false -v /var/run/docker.sock:/var/run/docker.sock feder8/install-script:2.0.0 feder8 init zeppelin
+docker run --rm -it --network feder8-net -e CURRENT_DIRECTORY=$(pwd) -e IS_WINDOWS=false -v /var/run/docker.sock:/var/run/docker.sock harbor.honeur.org/library/install-script:2.0.0 feder8 init zeppelin
