1d0f3322eee86055d8894592b6dcf1ddb374356c
Sus Verwimp <sus.verwimp@hotmail.com>
2021-08-02 14:39:22 +0200

    pull image if newer is available

--- remote-installation/helper-scripts/start-feder8-full.sh
+++ remote-installation/helper-scripts/start-feder8-full.sh
@@ -5,4 +5,5 @@ else
 fi
 
 docker network create feder8-net >/dev/null 2>&1 || true
+docker pull harbor.honeur.org/library/install-script:2.0.0
 docker run --rm -it --network feder8-net -e CURRENT_DIRECTORY=$(pwd) -e IS_WINDOWS=false -e DOCKER_CERT_SUPPORT=$DOCKER_CERT_SUPPORT -v /var/run/docker.sock:/var/run/docker.sock harbor.honeur.org/library/install-script:2.0.0 feder8 init full
--- remote-installation/separate-scripts/start-local-portal.cmd
+++ remote-installation/separate-scripts/start-local-portal.cmd
@@ -1,4 +1,5 @@
 @ECHO off
 
 docker network create feder8-net
+docker pull harbor.honeur.org/library/install-script:2.0.0
 docker run --rm -it --network feder8-net -v /var/run/docker.sock:/var/run/docker.sock harbor.honeur.org/library/install-script:2.0.0 feder8 init local-portal
