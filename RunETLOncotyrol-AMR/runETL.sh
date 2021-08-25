6688f4b385ade032ccea31a7f35c539ec80dafb2
freija-edence <freija.descamps@edence.health>
2021-08-25 14:58:08 +0200

    [HO-56] Set up deployment script for Oncotryrol AMT ETL

---
+++ RunETLOncotyrol-AMR/runETL.sh
@@ -0,0 +1,29 @@
+if [ $(docker ps --filter "name=etl" | grep -w 'etl' | wc -l) = 1 ]; then
+  docker stop -t 1 etl && docker rm etl;
+fi
+
+#curl -L https://raw.githubusercontent.com/solventrix/Honeur-Setup/master/RunETLOncotyrol-AMR/docker-compose.yml --output docker-compose.yml
+
+read -p "Input Data folder [./data]: " data_folder
+data_folder=${data_folder:-./data}
+read -p "DB username [honeur_admin]: " db_username
+db_username=${db_username:-honeur_admin}
+read -p "DB password [honeur_admin]: " db_password
+db_password=${db_password:-honeur_admin}
+read -p "Output verbosity level [INFO]: " verbosity_level
+verbosity_level=${verbosity_level:-INFO}
+read -p "Docker Hub image tag [current]: " image_tag
+image_tag=${image_tag:-current}
+read -p "Filename of input data [amr.csv]: " filename
+filename=${filename:-amr.csv}
+
+sed -i -e "s@data_folder@$data_folder@g" docker-compose.yml
+sed -i -e "s/db_username/$db_username/g" docker-compose.yml
+sed -i -e "s/db_password/$db_password/g" docker-compose.yml
+sed -i -e "s/verbosity_level/$verbosity_level/g" docker-compose.yml
+sed -i -e "s/image_tag/$image_tag/g" docker-compose.yml
+sed -i -e "s/filename/$filename/g" docker-compose.yml
+
+docker login harbor.honeur.org
+docker-compose pull
+docker-compose run --rm --name etl etl
