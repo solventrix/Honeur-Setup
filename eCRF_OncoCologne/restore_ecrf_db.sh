6b682ffa41ba395f4846f6afce44a70d00a7377f
petermoorthamer <pmoorth1@its.jnj.com>
2021-09-27 18:05:52 +0200

    Patient Data Entry tool: installation script for OncoCologne (JIRA AEKR-1368)

---
+++ eCRF_OncoCologne/restore_ecrf_db.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+set -e
+
+if [ -z "$1" ]
+  then
+    echo "Please specfy the file to restore"
+    exit 1
+fi
+
+POSTGRES_PASSWORD=gyZcrz9Zzp@8waX8
+DATABASE_BACKUP_FILE=$1
+CURRENT_TIME=$(date "+%Y_%m_%d_%H_%M_%S")
+RESTORE_FOLDER=${PWD}/restore/${CURRENT_TIME}
+
+mkdir -p ${RESTORE_FOLDER}
+
+export LANG=en_US.UTF-8
+export LC_ALL=$LANG
+tar -C ${RESTORE_FOLDER} -zxvf ${DATABASE_BACKUP_FILE}
+
+echo "remove old database container"
+docker stop honeur_ecrf_postgres
+docker rm honeur_ecrf_postgres
+docker volume rm postgres_data
+docker volume create postgres_data
+echo "re-create database container"
+docker run -d --name honeur_ecrf_postgres --network honeur-net --volume postgres_data:/var/lib/postgresql/data --env POSTGRES_PASSWORD=$POSTGRES_PASSWORD --restart=always -p 5432:5432 harbor.honeur.org/ecrf/postgres:0.2
+sleep 5s
+
+for sql_script in ${RESTORE_FOLDER}/*.sql; do
+    sql_script_name=$(basename -- "$sql_script")
+    DB_NAME="${sql_script_name%%.*}"
+    echo "Restore database $DB_NAME"
+    cat $sql_script | docker exec -i honeur_ecrf_postgres bash -c "PGPASSWORD=${POSTGRES_PASSWORD} psql -U postgres -d $DB_NAME"
+done
+
+rm -rf ${RESTORE_FOLDER}
