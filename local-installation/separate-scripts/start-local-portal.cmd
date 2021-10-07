2795b9cf67af3024d39f84a4b10af70239817f83
Sus Verwimp <sus.verwimp@hotmail.com>
2021-10-07 17:15:08 +0200

    use is_windows for local-portal
    (cherry picked from commit 5ac8b364da2b3ae80fa7a3b36eeba8d3ede04e82)

--- local-installation/separate-scripts/start-local-portal.cmd
+++ local-installation/separate-scripts/start-local-portal.cmd
@@ -4,4 +4,4 @@ SET TAG=2.0.1
 SET REGISTRY=harbor.honeur.org
 
 docker pull %REGISTRY%/library/install-script:%TAG%
-docker run --rm -it --name feder8-installer -v /var/run/docker.sock:/var/run/docker.sock %REGISTRY%/library/install-script:%TAG% feder8 init local-portal
+docker run --rm -it --name feder8-installer -e IS_WINDOWS=true -v /var/run/docker.sock:/var/run/docker.sock %REGISTRY%/library/install-script:%TAG% feder8 init local-portal
