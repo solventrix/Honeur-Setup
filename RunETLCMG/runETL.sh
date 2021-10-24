f8b63b94dc1fe3b923961004b8c94a8647aa530f
freija-edence <freija.descamps@edence.health>
2021-10-24 10:15:58 +0200

    Update RunETLCMG to use Harbor

--- RunETLCMG/runETL.sh
+++ RunETLCMG/runETL.sh
@@ -1,9 +1,6 @@
-docker stop run-python-etl
-docker rm run-python-etl
-
-rm -rf ./CMG
-
-git clone https://github.com/solventrix/CMG ./CMG
+if [ $(docker ps --filter "name=etl" | grep -w 'etl' | wc -l) = 1 ]; then
+  docker stop -t 1 etl && docker rm etl;
+fi
 
 curl -L https://raw.githubusercontent.com/solventrix/Honeur-Setup/master/RunETLCMG/docker-compose.yml --output docker-compose.yml
 
@@ -13,9 +10,19 @@ read -p "DB username [honeur_admin]: " db_username
 db_username=${db_username:-honeur_admin}
 read -p "DB password [honeur_admin]: " db_password
 db_password=${db_password:-honeur_admin}
+read -p "Docker Hub image tag [current]: " image_tag
+image_tag=${image_tag:-current}
+read -p "Date of last export yyyy-mm-dd [\"2021-06-01\"]: " date_last_export
+date_last_export=${date_last_export:-\"2021-06-01\"}
 
 sed -i -e "s@data_folder@$data_folder@g" docker-compose.yml
 sed -i -e "s/db_username/$db_username/g" docker-compose.yml
 sed -i -e "s/db_password/$db_password/g" docker-compose.yml
+sed -i -e "s/image_tag/$image_tag/g" docker-compose.yml
+sed -i -e "s/date_last_export/$date_last_export/g" docker-compose.yml
 
-docker-compose up
+echo "Logging into Harbor"
+docker login harbor.honeur.org
+docker-compose pull
+echo "Executing ETL"
+docker-compose run --rm --name etl etl
