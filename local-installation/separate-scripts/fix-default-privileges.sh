74f481c27e9f21e3cea804f070f3fcde74a531b6
petermoorthamer <pmoorth1@its.jnj.com>
2022-01-11 17:30:11 +0100

    Upgrade installation script to v2.0.13 (JIRA AEKR-1444)

--- local-installation/separate-scripts/fix-default-privileges.sh
+++ local-installation/separate-scripts/fix-default-privileges.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-TAG=2.0.12
+TAG=2.0.13
 REGISTRY=harbor-uat.honeur.org
 
 if systemctl show --property ActiveState docker &> /dev/null; then
--- local-installation/separate-scripts/update-postgres-to-v13.sh
+++ local-installation/separate-scripts/update-postgres-to-v13.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
-TAG=2.0.12
+TAG=2.0.13
 REGISTRY=harbor-uat.honeur.org
 
 docker volume create new-pgdata
