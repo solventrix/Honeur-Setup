9cc212091873c947cf1043f6a51dfc3a8c4b2d5f
petermoorthamer <pmoorth1@its.jnj.com>
2022-03-23 11:29:25 +0100

    Add separate install script for Disease Explorer (JIRA AEKR-1496)

---
+++ local-installation/separate-scripts/start-disease-explorer.cmd
@@ -0,0 +1,6 @@
+@ECHO off
+SET TAG=2.0.17
+SET REGISTRY=harbor-dev.honeur.org
+
+docker pull %REGISTRY%/library/install-script:%TAG%
+docker run --rm -it --name feder8-installer -e IS_WINDOWS=true -v /var/run/docker.sock:/var/run/docker.sock %REGISTRY%/library/install-script:%TAG% feder8 init disease_explorer
