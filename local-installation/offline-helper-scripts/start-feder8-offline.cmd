7d34be1547daf2bd48580ff2a5aaf8a627894722
Shawn-Ross Gyina <sgyina1@its.jnj.com>
2022-10-17 16:49:31 +0200

    AEKR-1622: Added images.tar check in offline scripts

--- local-installation/offline-helper-scripts/start-feder8-offline.cmd
+++ local-installation/offline-helper-scripts/start-feder8-offline.cmd
@@ -5,6 +5,11 @@ SET REGISTRY=harbor-uat.honeur.org
 
 docker logout %REGISTRY%
 
-echo Loading docker images. This could take a while...
-docker load -i images.tar
-docker run --rm -it --name feder8-installer -e CURRENT_DIRECTORY=%CD% -e IS_WINDOWS=true -e DOCKER_CERT_SUPPORT=false -v /var/run/docker.sock:/var/run/docker.sock %REGISTRY%/library/install-script:%TAG% feder8 init full --offline
+if exist images.tar (
+    echo Loading docker images. This could take a while...
+    docker load -i images.tar
+    docker run --rm -it --name feder8-installer -e CURRENT_DIRECTORY=%CD% -e IS_WINDOWS=true -e DOCKER_CERT_SUPPORT=false -v /var/run/docker.sock:/var/run/docker.sock %REGISTRY%/library/install-script:%TAG% feder8 init full --offline
+) else (
+    echo Could not find 'images.tar' in the current directory. Unable to continue.
+    exit 1
+)
--- local-installation/offline-helper-scripts/start-feder8-offline.sh
+++ local-installation/offline-helper-scripts/start-feder8-offline.sh
@@ -15,6 +15,11 @@ else
   IS_MAC=false
 fi
 
-echo Loading docker images. This could take a while...
-docker load < images.tar
-docker run --rm -it --name feder8-installer -e CURRENT_DIRECTORY=$(pwd) -e IS_WINDOWS=false -e IS_MAC=$IS_MAC -e DOCKER_CERT_SUPPORT=$DOCKER_CERT_SUPPORT -v /var/run/docker.sock:/var/run/docker.sock ${REGISTRY}/library/install-script:${TAG} feder8 init full --offline
+if [ -f "images.tar" ]; then
+  echo Loading docker images. This could take a while...
+  docker load < images.tar
+  docker run --rm -it --name feder8-installer -e CURRENT_DIRECTORY=$(pwd) -e IS_WINDOWS=false -e IS_MAC=$IS_MAC -e DOCKER_CERT_SUPPORT=$DOCKER_CERT_SUPPORT -v /var/run/docker.sock:/var/run/docker.sock ${REGISTRY}/library/install-script:${TAG} feder8 init full --offline
+else
+  echo Could not find 'images.tar' in the current directory. Unable to continue.
+  exit 1
+fi
