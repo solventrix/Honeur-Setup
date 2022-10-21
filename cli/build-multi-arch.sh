ad211a676979c85e08c451378581d115fc37b33d
Peter Moorthamer <moorthamer@gmail.com>
2022-10-21 09:01:07 +0200

    The install script is shared between all TA's

--- cli/build-multi-arch.sh
+++ cli/build-multi-arch.sh
@@ -4,19 +4,6 @@ set -eux
 VERSION=2.0.19
 TAG=$VERSION
 
-REPOSITORY=library
-
 REGISTRY=harbor.honeur.org
-docker buildx build --rm --platform linux/amd64,linux/arm64 --no-cache --pull --push -f "Dockerfile" -t $REGISTRY/$REPOSITORY/install-script:$TAG "."
-
-REGISTRY=harbor.athenafederation.org
-docker buildx build --rm --platform linux/amd64,linux/arm64 --no-cache --pull --push -f "Dockerfile" -t $REGISTRY/$REPOSITORY/install-script:$TAG "."
-
-REGISTRY=harbor.lupusnet.org
-docker buildx build --rm --platform linux/amd64,linux/arm64 --no-cache --pull --push -f "Dockerfile" -t $REGISTRY/$REPOSITORY/install-script:$TAG "."
-
-REGISTRY=harbor.esfurn.org
-docker buildx build --rm --platform linux/amd64,linux/arm64 --no-cache --pull --push -f "Dockerfile" -t $REGISTRY/$REPOSITORY/install-script:$TAG "."
-
-REGISTRY=harbor.phederation.org
+REPOSITORY=library
 docker buildx build --rm --platform linux/amd64,linux/arm64 --no-cache --pull --push -f "Dockerfile" -t $REGISTRY/$REPOSITORY/install-script:$TAG "."
