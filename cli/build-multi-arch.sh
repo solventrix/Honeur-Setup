79a00f582c8a4afa3ec1c7923e02e6add2359847
Shawn <35766067+ShawnRG@users.noreply.github.com>
2022-11-15 17:05:12 +0100

    Update build-multi-arch.sh

--- cli/build-multi-arch.sh
+++ cli/build-multi-arch.sh
@@ -3,7 +3,5 @@ set -eux
 
 VERSION=2.0.19
 TAG=$VERSION
-REGISTRY=harbor-uat.honeur.org
-REPOSITORY=library
 
-docker buildx build --rm --platform linux/amd64,linux/arm64 --no-cache --pull --push -f "Dockerfile" -t $REGISTRY/$REPOSITORY/install-script:$TAG "."
+docker buildx build --rm --platform linux/amd64,linux/arm64 --no-cache --pull --push -f "Dockerfile" -t $THERPEUTIC_AREA_URL/library/install-script:$TAG "."
