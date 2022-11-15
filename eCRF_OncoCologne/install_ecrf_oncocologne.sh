59fd0c87fff4fa62c4361afe557a14aeca43e89e
Peter Moorthamer <moorthamer@gmail.com>
2022-11-15 21:20:14 +0100

    Script improvements

--- eCRF_OncoCologne/install_ecrf_oncocologne.sh
+++ eCRF_OncoCologne/install_ecrf_oncocologne.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 set -e
 
+REGISTRY=harbor.honeur.org
+SITE=oncocologne
+REPOSITORY=ecrf/${SITE}
+VERSION=0.2
+NETWORK=honeur-net
 DATABASE_NAME=opal
 
 POSTGRES_PASSWORD=
@@ -23,19 +28,19 @@ if [[ $(docker volume inspect postgres_data | grep -q 'Error') != 0 ]]; then
 fi
 
 echo "pull docker images"
-docker pull harbor.honeur.org/ecrf/oncocologne/postgres:0.2
-docker pull harbor.honeur.org/ecrf/oncocologne/app:0.2
-docker pull harbor.honeur.org/ecrf/oncocologne/nginx:0.2
+docker pull $REGISTRY/$REPOSITORY/postgres:$VERSION
+docker pull $REGISTRY/$REPOSITORY/app:$VERSION
+docker pull $REGISTRY/$REPOSITORY/nginx:$VERSION
 
-echo "create network honeur-net"
-docker network create honeur-net || true
+echo "create network $NETWORK"
+docker network create $NETWORK || true
 
 echo "create volumes postgres_data and static_volume"
 docker volume create postgres_data
 docker volume create static_volume
 
 echo "create database container"
-docker run -d --name honeur_ecrf_postgres --network honeur-net --volume postgres_data:/var/lib/postgresql/data --env POSTGRES_PASSWORD=$POSTGRES_PASSWORD --restart=always harbor.honeur.org/ecrf/oncocologne/postgres:0.2
+docker run -d --name honeur_ecrf_postgres --network $NETWORK -p 5432:5432 --volume postgres_data:/var/lib/postgresql/data --env POSTGRES_PASSWORD=$POSTGRES_PASSWORD --restart=always $REGISTRY/$REPOSITORY/postgres:$VERSION
 echo "waiting on database container..."
 i=1
 while [[ $i -lt 30 ]] ; do
@@ -50,7 +55,7 @@ echo "create eCRF app container"
 if $fresh_install; then
   echo "Fresh install"
   echo "Initializing.  This will take some time..."
-  docker run -d --name honeur_ecrf_app --network honeur-net --volume static_volume:/code/entrytool/assets --env OPAL_SUPER_USER_PASSWORD=$OPAL_SUPER_USER_PASSWORD --env OPAL_DB_USER=postgres --env OPAL_DB_PASSWORD=$POSTGRES_PASSWORD --env OPAL_DB_NAME=$DATABASE_NAME --env OPAL_DB_HOST=honeur_ecrf_postgres --env OPAL_DB_PORT=5432 --env OPAL_FLUSH_DB=true --env OPAL_ENABLE_USER_DB=false --env OPAL_ENABLE_LDAP=false harbor.honeur.org/ecrf/oncocologne/app:0.2 gunicorn -b 0.0.0.0:8000 entrytool.wsgi
+  docker run -d --name honeur_ecrf_app --network $NETWORK --volume static_volume:/code/entrytool/assets --env OPAL_SUPER_USER_PASSWORD=$OPAL_SUPER_USER_PASSWORD --env OPAL_DB_USER=postgres --env OPAL_DB_PASSWORD=$POSTGRES_PASSWORD --env OPAL_DB_NAME=$DATABASE_NAME --env OPAL_DB_HOST=honeur_ecrf_postgres --env OPAL_DB_PORT=5432 --env OPAL_FLUSH_DB=true --env OPAL_ENABLE_USER_DB=false --env OPAL_ENABLE_LDAP=false $REGISTRY/$REPOSITORY/app:$VERSION gunicorn -b 0.0.0.0:8000 entrytool.wsgi
   i=1
   while [[ $i -lt 120 ]] ; do
      printf "."
@@ -65,7 +70,7 @@ fi
 printf "\n"
 
 echo "start eCRF app container"
-docker run -d --name honeur_ecrf_app --network honeur-net --volume static_volume:/code/entrytool/assets --env OPAL_SUPER_USER_PASSWORD=$OPAL_SUPER_USER_PASSWORD --env OPAL_DB_USER=postgres --env OPAL_DB_PASSWORD=$POSTGRES_PASSWORD --env OPAL_DB_NAME=$DATABASE_NAME --env OPAL_DB_HOST=honeur_ecrf_postgres --env OPAL_DB_PORT=5432 --env OPAL_FLUSH_DB=false --env OPAL_ENABLE_USER_DB=false --env OPAL_ENABLE_LDAP=false --restart=always harbor.honeur.org/ecrf/oncocologne/app:0.2 gunicorn -b 0.0.0.0:8000 entrytool.wsgi
+docker run -d --name honeur_ecrf_app --network $NETWORK --volume static_volume:/code/entrytool/assets --env OPAL_SUPER_USER_PASSWORD=$OPAL_SUPER_USER_PASSWORD --env OPAL_DB_USER=postgres --env OPAL_DB_PASSWORD=$POSTGRES_PASSWORD --env OPAL_DB_NAME=$DATABASE_NAME --env OPAL_DB_HOST=honeur_ecrf_postgres --env OPAL_DB_PORT=5432 --env OPAL_FLUSH_DB=false --env OPAL_ENABLE_USER_DB=false --env OPAL_ENABLE_LDAP=false --restart=always $REGISTRY/$REPOSITORY/app:$VERSION gunicorn -b 0.0.0.0:8000 entrytool.wsgi
 i=1
 while [[ $i -lt 20 ]] ; do
    printf "."
@@ -74,5 +79,5 @@ while [[ $i -lt 20 ]] ; do
 done
 printf "\n"
 
-echo "create nginx container"
-docker run -d --name honeur_ecrf_nginx --network honeur-net -v static_volume:/code/entrytool/assets --restart=always -p 80:80 harbor.honeur.org/ecrf/oncocologne/nginx:0.2
+echo "create NGINX container"
+docker run -d --name honeur_ecrf_nginx --network $NETWORK -v static_volume:/code/entrytool/assets --restart=always -p 80:80 $REGISTRY/$REPOSITORY/nginx:$VERSION
