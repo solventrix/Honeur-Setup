3ddbbe2259a669cfb02c2cf59814837ec9b2cce0
Shawn <35766067+ShawnRG@users.noreply.github.com>
2022-11-16 11:53:14 +0100

    Update build-multi-arch.sh

--- cli/build-multi-arch.sh
+++ cli/build-multi-arch.sh
@@ -4,4 +4,4 @@ set -eux
 VERSION=2.0.19
 TAG=$VERSION
 
-docker buildx build --platform linux/amd64,linux/arm64 --no-cache --pull --push -f "Dockerfile" -t $THERAPEUTIC_AREA_URL/library/install-script:$TAG "."
+docker buildx build --rm --platform linux/amd64,linux/arm64 --no-cache --pull --push -f "Dockerfile" -t $THERAPEUTIC_AREA_URL/library/install-script:$TAG "."
