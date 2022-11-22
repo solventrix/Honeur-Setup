0dcd350af62c443f24d445ccfd8d2b46a7c0f6ac
Peter Moorthamer <moorthamer@gmail.com>
2022-11-22 09:16:33 +0100

    Improvement

--- cli/build-multi-arch.sh
+++ cli/build-multi-arch.sh
@@ -3,5 +3,7 @@ set -eux
 
 VERSION=2.0.20
 TAG=$VERSION
+THERAPEUTIC_AREA_URL="${THERAPEUTIC_AREA_URL:=harbor-uat.honeur.org}"
+REPOSITORY=library
 
-docker buildx build --rm --platform linux/amd64,linux/arm64 --no-cache --pull --push -f "Dockerfile" -t $THERAPEUTIC_AREA_URL/library/install-script:$TAG "."
+docker buildx build --rm --platform linux/amd64,linux/arm64 --no-cache --pull --push -f "Dockerfile" -t $THERAPEUTIC_AREA_URL/$REPOSITORY/install-script:$TAG "."
