2bbb0c10fbb4793ec2445c4448831a352e76848b
Peter Moorthamer <moorthamer@gmail.com>
2023-01-19 10:30:55 +0100

    CLI for Feder8 local installation moved to new Github repo: https://github.com/Feder8-Platform/Feder8-Local-CLI (JIRA AEKR-1744)

--- cli/build-multi-arch.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -eux
-
-VERSION=2.0.21
-TAG=$VERSION
-THERAPEUTIC_AREA_URL="${THERAPEUTIC_AREA_URL:=harbor.honeur.org}"
-REPOSITORY=library
-
-docker buildx build --rm --platform linux/amd64,linux/arm64 --no-cache --pull --push -f "Dockerfile" -t $THERAPEUTIC_AREA_URL/$REPOSITORY/install-script:$TAG "."
--- cli/cli/registry/registry.py
+++
@@ -1,4 +0,0 @@
-class Registry:
-    def __init__(self, registry_url:str, project:str) -> None:
-        self.registry_url = registry_url
-        self.project = project
