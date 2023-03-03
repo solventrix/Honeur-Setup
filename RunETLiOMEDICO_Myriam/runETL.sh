c878c747cef932886dbd4447776099c1764289d8
Lars Halvorsen <lhalvors@users.noreply.github.com>
2023-03-03 11:36:22 +0100

    Require date of last export to be entered manually

--- RunETLiOMEDICO_Myriam/runETL.sh
+++ RunETLiOMEDICO_Myriam/runETL.sh
@@ -14,8 +14,9 @@ read -p "Output verbosity level [INFO]: " verbosity_level
 verbosity_level=${verbosity_level:-INFO}
 read -p "Docker Hub image tag [current]: " image_tag
 image_tag=${image_tag:-current}
-read -p "Date of last export yyyy-mm-dd [\"2021-06-01\"]: " date_last_export
-date_last_export=${date_last_export:-\"2021-06-01\"}
+until read -r -p "Date of last export yyyy-mm-dd: " date_last_export && test "$date_last_export" != ""; do
+  continue
+done
 
 sed -i -e "s@data_folder@$data_folder@g" docker-compose.yml
 sed -i -e "s/db_username/$db_username/g" docker-compose.yml
