daf506899d2be038a96c3fc0422c6fb43bebfb49
Shawn <35766067+ShawnRG@users.noreply.github.com>
2023-03-08 15:38:31 +0100

    Update start-local-portal.sh

--- local-installation/separate-scripts/start-local-portal.sh
+++ local-installation/separate-scripts/start-local-portal.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-TAG=2.0.21
+TAG=2.0.22
 REGISTRY=harbor.honeur.org
 
 if [[ $OSTYPE == 'darwin'* ]]; then
@@ -9,5 +9,7 @@ else
   IS_MAC=false
 fi
 
+BRIDGE_GATEWAY_IP=$(docker network inspect bridge -f "{{ (index .IPAM.Config 0).Gateway }}")
+
 docker pull ${REGISTRY}/library/install-script:${TAG}
-docker run --rm -it --name feder8-installer -e IS_MAC=$IS_MAC -v /var/run/docker.sock:/var/run/docker.sock ${REGISTRY}/library/install-script:${TAG} feder8 init local-portal
+docker run --rm -it --name feder8-installer -e IS_MAC=$IS_MAC -e FEDER8_INSTALL_DOCKER_HOST=$BRIDGE_GATEWAY_IP -v /var/run/docker.sock:/var/run/docker.sock ${REGISTRY}/library/install-script:${TAG} feder8 init local-portal
