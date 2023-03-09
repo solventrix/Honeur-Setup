f512079ecd9e14a623e0c708a72d585eee28b77c
Peter Moorthamer <moorthamer@gmail.com>
2023-03-09 08:15:25 +0100

    Add update script for the Docker policies

---
+++ local-installation/separate-scripts/update-docker-policies.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+read -p "Enter the folder containing the certificates [$PWD/certificates]: " CERTIFICATE_FOLDER
+CERTIFICATE_FOLDER=${CERTIFICATE_FOLDER:-$PWD/certificates}
+
+if [ ! -d "$CERTIFICATE_FOLDER" ]; then
+    echo "Warning: '$CERTIFICATE_FOLDER' NOT found.  Abort."
+    exit
+fi
+
+# Create policy file
+echo "Update policy file"
+echo -n "" > $CERTIFICATE_FOLDER/policy.json
+echo '{"name":"full-access","users":["", "host"],"actions":[""]}' >> $CERTIFICATE_FOLDER/policy.json
+echo '{"name":"feder8-studio","users":["feder8"],"actions":["container_create","container_inspect","container_list","container_logs","container_start","container_delete","docker_version","image_list","image_create","network_connect","network_disconnect"]}' >> $CERTIFICATE_FOLDER/policy.json
