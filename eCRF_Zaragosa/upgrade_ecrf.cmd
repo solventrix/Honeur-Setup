472c4f084b5baee1e1a2af4d450baa9df74cddd1
Peter Moorthamer <moorthamer@gmail.com>
2023-03-16 08:44:49 +0100

    Add upgrade script for eCRF Zaragosa (AEKR-1798)

---
+++ eCRF_Zaragosa/upgrade_ecrf.cmd
@@ -0,0 +1,36 @@
+@echo off
+set -e
+
+SET REGISTRY=harbor.honeur.org
+SET SITE=zaragosa
+SET REPOSITORY=ecrf/%SITE%
+SET VERSION=0.2.1
+SET NETWORK=feder8-net
+SET DATABASE_NAME=postgres
+
+SET /p POSTGRES_PASSWORD="Please enter a strong password for Postgres: "
+:while-postgres-password-not-correct
+if "%POSTGRES_PASSWORD%" == "" (
+   echo The password cannot be empty
+   SET /p POSTGRES_PASSWORD="Please enter a strong password for Postgres: "
+   goto :while-postgres-password-not-correct
+)
+
+SET /p OPAL_SUPER_USER_PASSWORD="Please enter a strong password for the 'super' user of the eCRF tool: "
+:while-ecrf-password-not-correct
+if "%POSTGRES_PASSWORD%" == "" (
+   echo The password cannot be empty
+   SET /p OPAL_SUPER_USER_PASSWORD="Please enter a strong password for the 'super' user of the eCRF tool: "
+   goto :while-ecrf-password-not-correct
+)
+
+echo "pull docker image"
+docker pull %REGISTRY%/%REPOSITORY%/app:%VERSION%
+
+echo "stop and remove existing eCRF app container"
+docker stop ecrf-app
+docker rm ecrf-app
+
+echo "create new eCRF app container"
+docker run -d --name ecrf-app --network %NETWORK% --volume static_volume:/code/entrytool/assets --env OPAL_SUPER_USER_PASSWORD=%OPAL_SUPER_USER_PASSWORD% --env OPAL_DB_USER=postgres --env OPAL_DB_PASSWORD=%POSTGRES_PASSWORD% --env OPAL_DB_NAME=%DATABASE_NAME% --env OPAL_DB_HOST=ecrf-postgres --env OPAL_DB_PORT=5432 --env OPAL_FLUSH_DB=false --env OPAL_ENABLE_USER_DB=false --env OPAL_ENABLE_LDAP=false --restart=always %REGISTRY%/%REPOSITORY%/app:%VERSION% gunicorn -b 0.0.0.0:8000 --timeout 300 entrytool.wsgi
+TIMEOUT 20
---
+++ eCRF_Zaragosa/upgrade_ecrf.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+set -e
+
+REGISTRY=harbor.honeur.org
+SITE=zaragosa
+REPOSITORY=ecrf/${SITE}
+VERSION=0.2.1
+NETWORK=feder8-net
+DATABASE_NAME=postgres
+
+POSTGRES_PASSWORD=
+read -p "Please enter the password for Postgres: " POSTGRES_PASSWORD
+while [[ "$POSTGRES_PASSWORD" == "" ]]; do
+    echo "The password cannot be empty"
+    read -p "Please enter the password for Postgres: " POSTGRES_PASSWORD
+done
+
+OPAL_SUPER_USER_PASSWORD=
+read -p "Please enter the password for the 'super' user of the eCRF tool: " OPAL_SUPER_USER_PASSWORD
+while [[ "$OPAL_SUPER_USER_PASSWORD" == "" ]]; do
+    echo "The password cannot be empty"
+    read -p "Please enter the password for the 'super' user of the eCRF tool: " OPAL_SUPER_USER_PASSWORD
+done
+
+echo "pull docker image"
+docker pull $REGISTRY/$REPOSITORY/app:$VERSION
+
+echo "stop and remove existing eCRF app container"
+docker stop ecrf-app
+docker rm ecrf-app
+
+echo "create new eCRF app container"
+docker run -d --name ecrf-app --network $NETWORK --volume static_volume:/code/entrytool/assets --env OPAL_SUPER_USER_PASSWORD=$OPAL_SUPER_USER_PASSWORD --env OPAL_DB_USER=postgres --env OPAL_DB_PASSWORD=$POSTGRES_PASSWORD --env OPAL_DB_NAME=$DATABASE_NAME --env OPAL_DB_HOST=ecrf-postgres --env OPAL_DB_PORT=5432 --env OPAL_FLUSH_DB=false --env OPAL_ENABLE_USER_DB=false --env OPAL_ENABLE_LDAP=false --restart=always $REGISTRY/$REPOSITORY/app:$VERSION gunicorn -b 0.0.0.0:8000 --timeout 300 entrytool.wsgi
+i=1
+while [[ $i -lt 20 ]] ; do
+   printf "."
+   sleep 1
+  (( i += 1 ))
+done
+printf "\n"
