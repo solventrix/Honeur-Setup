9348318738c9121eb74275103601be42534e01ea
Lars Halvorsen <lhalvors@users.noreply.github.com>
2023-05-29 12:10:38 +0200

    Update runETL.sh
    Update default file names

--- RunETLRHEMCO/runETL.sh
+++ RunETLRHEMCO/runETL.sh
@@ -14,12 +14,12 @@ read -p "Output verbosity level [INFO]: " verbosity_level
 verbosity_level=${verbosity_level:-INFO}
 read -p "Docker Hub image tag [current]: " image_tag
 image_tag=${image_tag:-current}
-read -p "Molecule source file name [1_rhemco_21_tt_molecule_1980_2018.txt]: " mol_file
-mol_file=${mol_file:-1_rhemco_21_tt_molecule_1980_2018.txt}
-read -p "Hemo source file name [rhemco_21_hemo_1980_2018.txt]: " hemo_file
-hemo_file=${hemo_file:-rhemco_21_hemo_1980_2018.txt}
-read -p "Indiv source file name [rhemco_21_indiv_1980_2018.txt]: " indiv_file
-indiv_file=${indiv_file:-rhemco_21_indiv_1980_2018.txt}
+read -p "Molecule source file name [1_rhemco_21_tt_molecule_1980_2020.txt]: " mol_file
+mol_file=${mol_file:-1_rhemco_21_tt_molecule_1980_2020.txt}
+read -p "Hemo source file name [rhemco_21_hemo_1980_2020.txt]: " hemo_file
+hemo_file=${hemo_file:-rhemco_21_hemo_1980_2020.txt}
+read -p "Indiv source file name [rhemco_21_indiv_1980_2020.txt]: " indiv_file
+indiv_file=${indiv_file:-rhemco_21_indiv_1980_2020.txt}
 file_names="${mol_file}:${hemo_file}:${indiv_file}"
 until read -r -p "Date of last export yyyy-mm-dd: " date_last_export && test "$date_last_export" != ""; do
   continue
