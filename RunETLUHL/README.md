52e89b94b6db8842ade710baa6d0ae409c2bee1a
Peter Moorthamer <moorthamer@gmail.com>
2023-06-05 09:05:37 +0200

    Use latest ETL runner for UHL

--- RunETLUHL/README.md
+++ RunETLUHL/README.md
@@ -1,21 +1,16 @@
 # How to execute the ETL for UHL
 
 ## Prerequisites
-1. Git client is installed
-2. Docker is installed 
-3. The user has (read) access to the UHL repository containing the ETL code on GitHub: https://github.com/solventrix/UHL
-4. The HONEUR OMOP CDM database is running in a Docker container named 'postgres':
-    * See https://github.com/solventrix/Honeur-Setup/blob/master/OMOPCDM/README.md for more info
-
+1. Docker is installed 
+2. The Feder8 OMOP CDM database is running in a Docker container named 'postgres':
+    
 ## Execution steps
 1. Open a terminal window 
 2. Download the installation script:
     * `curl -L https://raw.githubusercontent.com/solventrix/Honeur-Setup/master/RunETLUHL/runETL.sh --output runETL.sh && chmod +x runETL.sh`
 3. Execute the 'runETL.sh' script
 4. The script will request for:
-    * the folder, filename and sheet of the Excel input file containing the raw data   
-    * the hostname, port and name of the OMOP CDM database
-    * the username and password to connect to the OMOP CDM database
+    * the folder, filename and sheet of the Excel input file containing the raw data
 5. The script will run the ETL code and show the output of the code
-6. The log file will be available in the /UHL/UHL-ETL/logs folder
+6. The log file will be available in a 'logs' folder
 
--- RunETLUHL/runETL.sh
+++ RunETLUHL/runETL.sh
@@ -1,36 +1,48 @@
-docker stop run-etl-uhl
-docker rm run-etl-uhl
+#!/usr/bin/env bash
+set -ex
 
-rm -rf ./UHL
+REGISTRY=harbor.honeur.org
+REPOSITORY=library
+IMAGE=etl-runner
+VERSION=1.1.0
+TAG=$VERSION
 
-git clone https://github.com/solventrix/UHL ./UHL
+LOG_FOLDER_HOST=${PWD}/log
+QA_FOLDER_HOST=${PWD}/qa
 
-curl -L https://raw.githubusercontent.com/solventrix/Honeur-Setup/master/RunETLUHL/docker-compose.yml --output docker-compose.yml
+echo "Pull ETL runner image"
+docker pull $REGISTRY/$REPOSITORY/$IMAGE:$TAG
 
-read -p "Input data folder [./UHL/UHL-ETL/input]: " data_folder
-data_folder=${data_folder:-./UHL/UHL-ETL/input}
-read -p "Input data file [HONEUR_EXTRACT_20200306.xlsx]: " data_file
-data_file=${data_file:-HONEUR_EXTRACT_20200306.xlsx}
-read -p "Input data sheet [Sheet1]: " data_sheet
-data_sheet=${data_sheet:-Sheet1}
-read -p "DB host [postgres]: " db_host
-db_host=${db_host:-postgres}
-read -p "DB port [5432]: " db_port
-db_port=${db_port:-5432}
-read -p "DB name [OHDSI]: " db_name
-db_name=${db_name:-OHDSI}
-read -p "DB username [honeur_admin]: " db_username
-db_username=${db_username:-honeur_admin}
-read -p "DB password [honeur_admin]: " db_password
-db_password=${db_password:-honeur_admin}
+echo "Download ETL questions"
+curl -fsSL https://raw.githubusercontent.com/solventrix/Honeur-Setup/master/RunETLUHL/questions.json --output questions.json
 
-sed -i -e "s@<data_folder>@$data_folder@g" docker-compose.yml
-sed -i -e "s@<data_file>@$data_file@g" docker-compose.yml
-sed -i -e "s@<data_sheet>@$data_sheet@g" docker-compose.yml
-sed -i -e "s@<db_host>@$db_host@g" docker-compose.yml
-sed -i -e "s@<db_port>@$db_port@g" docker-compose.yml
-sed -i -e "s@<db_name>@$db_name@g" docker-compose.yml
-sed -i -e "s@<db_username>@$db_username@g" docker-compose.yml
-sed -i -e "s@<db_password>@$db_password@g" docker-compose.yml
+touch etl-runner.env
+echo "THERAPEUTIC_AREA=honeur" >> etl-runner.env
+echo "REGISTRY=$REGISTRY" >> etl-runner.env
+echo "LOG_LEVEL=INFO" >> etl-runner.env
+echo "LOG_FOLDER_HOST=$LOG_FOLDER_HOST" >> etl-runner.env
+echo "LOG_FOLDER=/var/log" >> etl-runner.env
+echo "ETL_IMAGE_NAME=etl-uhl/etl" >> etl-runner.env
+echo "ETL_IMAGE_TAG=1.3.0" >> etl-runner.env
+echo "DATA_FOLDER_HOST=$DATA_FOLDER_HOST" >> etl-runner.env
+echo "DATA_FOLDER=/etl/data" >> etl-runner.env
+echo "DATA_FILE=Honeur data 180922 UHL MG.xlsx" >> etl-runner.env
+echo "DATA_SHEET=Sheet1" >> etl-runner.env
+echo "QA_FOLDER_HOST=$QA_FOLDER_HOST" >> etl-runner.env
+#echo "SCRIPT_UUID=9719aeb1-84c4-49c5-a2a1-c6ea3af00305" >> etl-runner.env
+#echo "LAST_DATA_EXPORT=" >> etl-runner.env
+#echo "DATA_SET=" >> etl-runner.env
 
-docker-compose up
+echo "Run ETL"
+docker run \
+-it \
+--rm \
+--name etl-runner \
+--env-file etl-runner.env \
+-v /var/run/docker.sock:/var/run/docker.sock \
+-v ${PWD}/questions.json:/script/questions.json \
+--network feder8-net \
+$REGISTRY/$REPOSITORY/$IMAGE:$TAG
+
+echo "End of ETL run"
+rm -rf etl-runner.env
