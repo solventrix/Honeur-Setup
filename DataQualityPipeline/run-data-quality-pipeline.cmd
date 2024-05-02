8abc7c1b67b52663805e54faca57039ef1558aab
Peter Moorthamer <moorthamer@gmail.com>
2024-05-02 17:02:33 +0200

    Run latest version of the Data Quality Pipeline (JIRA AEKR-2049)

--- DataQualityPipeline/run-data-quality-pipeline.cmd
+++ DataQualityPipeline/run-data-quality-pipeline.cmd
@@ -3,7 +3,7 @@
 SET REGISTRY=harbor.honeur.org
 SET REPOSITORY=distributed-analytics
 SET IMAGE=data-quality-pipeline
-SET VERSION=1.6
+SET VERSION=1.7
 SET TAG=%VERSION%
 SET QA_FOLDER_HOST=%CD%/qa
 
@@ -12,4 +12,4 @@ docker login %REGISTRY%
 
 docker pull %REGISTRY%/%REPOSITORY%/%IMAGE%:%TAG%
 
-docker run --rm --name data-quality-pipeline --env REGISTRY=%REGISTRY% --env THERAPEUTIC_AREA=honeur --env INDICATION=mm --env QA_FOLDER_HOST=%QA_FOLDER_HOST% --env SCRIPT_UUID=9719aeb1-84c4-49c5-a2a1-c6ea3af00305 -v /var/run/docker.sock:/var/run/docker.sock --network feder8-net %REGISTRY%/%REPOSITORY%/%IMAGE%:%TAG%
+docker run --rm --name data-quality-pipeline --env REGISTRY=%REGISTRY% --env THERAPEUTIC_AREA=honeur --env INDICATION=mm --env QA_FOLDER_HOST=%QA_FOLDER_HOST% --env SCRIPT_UUID=0b77204e-bddf-4f40-a0de-9fe2d3fe8506 -v /var/run/docker.sock:/var/run/docker.sock --network feder8-net %REGISTRY%/%REPOSITORY%/%IMAGE%:%TAG%
--- DataQualityPipeline/run-data-quality-pipeline.sh
+++ DataQualityPipeline/run-data-quality-pipeline.sh
@@ -4,7 +4,7 @@ set -ex
 REGISTRY=harbor.honeur.org
 REPOSITORY=distributed-analytics
 IMAGE=data-quality-pipeline
-VERSION=1.6
+VERSION=1.7
 TAG=$VERSION
 
 echo "Docker login @ $REGISTRY"
@@ -19,7 +19,7 @@ echo "THERAPEUTIC_AREA=honeur" >> data-quality-pipeline.env
 echo "INDICATION=mm" >> data-quality-pipeline.env
 #echo "CDM_VERSION=5.4"  >> data-quality-pipeline.env
 echo "QA_FOLDER_HOST=$QA_FOLDER_HOST" >> data-quality-pipeline.env
-echo "SCRIPT_UUID=9719aeb1-84c4-49c5-a2a1-c6ea3af00305" >> data-quality-pipeline.env
+echo "SCRIPT_UUID=0b77204e-bddf-4f40-a0de-9fe2d3fe8506" >> data-quality-pipeline.env
 
 docker run \
 --rm \
