8aa4d33f63c1ffecd626578c9980d4eb0c2fbc31
Peter Moorthamer <moorthamer@gmail.com>
2024-06-13 14:28:00 +0200

    Add script to run nginx on a custom port

--- local-installation/separate-scripts/custom-http-port.sh
+++ local-installation/separate-scripts/custom-http-port.sh
@@ -17,6 +17,6 @@ fi
 echo "Pull latest local installation script"
 docker pull ${REGISTRY}/library/install-script:${TAG}
 echo "Upgrade local portal"
-docker run --rm -it --name feder8-installer -e IS_WINDOWS=false -e IS_MAC=$IS_MAC -e DOCKER_CERT_SUPPORT=$DOCKER_CERT_SUPPORT -v /var/run/docker.sock:/var/run/docker.sock ${REGISTRY}/library/install-script:${TAG} feder8 init local-portal
+docker run --rm -it --name feder8-installer -e CURRENT_DIRECTORY=${PWD} -e IS_WINDOWS=false -e IS_MAC=$IS_MAC -e DOCKER_CERT_SUPPORT=$DOCKER_CERT_SUPPORT -v /var/run/docker.sock:/var/run/docker.sock ${REGISTRY}/library/install-script:${TAG} feder8 init local-portal
 echo "Upgrade NGINX"
 docker run --rm -it --name feder8-installer -e CURRENT_DIRECTORY=${PWD} -e FEDER8_HOST_HTTP_PORT=8080 -e FEDER8_HOST_HTTPS_PORT=8443 -v /var/run/docker.sock:/var/run/docker.sock ${REGISTRY}/library/install-script:${TAG} feder8 init nginx
