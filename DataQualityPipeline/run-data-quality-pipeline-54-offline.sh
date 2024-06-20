1c31ffe29124d7cdf04162753f691647db42f885
Peter Moorthamer <moorthamer@gmail.com>
2024-06-20 17:33:56 +0200

    Data Quality Pipeline - add offline mode run script (JIRA AEKR-2074)

---
+++ DataQualityPipeline/run-data-quality-pipeline-54-offline.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+set -ex
+
+REGISTRY=harbor.honeur.org
+REPOSITORY=distributed-analytics
+IMAGE=data-quality-pipeline
+VERSION=1.8.1
+TAG=$VERSION
+
+docker load --input DQP-v1.8.1.tar.gz
+QA_FOLDER_HOST=${PWD}/qa
+
+touch data-quality-pipeline.env
+echo "REGISTRY=$REGISTRY" >> data-quality-pipeline.env
+echo "THERAPEUTIC_AREA=honeur" >> data-quality-pipeline.env
+echo "INDICATION=mm" >> data-quality-pipeline.env
+echo "CDM_VERSION=5.4"  >> data-quality-pipeline.env
+echo "QA_FOLDER_HOST=$QA_FOLDER_HOST" >> data-quality-pipeline.env
+echo "OFFLINE_MODE=true" >> data-quality-pipeline.env
+
+docker run \
+--rm \
+--name data-quality-pipeline \
+--env-file data-quality-pipeline.env \
+-v /var/run/docker.sock:/var/run/docker.sock \
+--network feder8-net \
+$REGISTRY/$REPOSITORY/$IMAGE:$TAG
+
+rm -rf data-quality-pipeline.env
