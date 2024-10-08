abe4788ec1e32b14f0c5a2d317e3dcf4a931201f
Peter Moorthamer <moorthamer@gmail.com>
2024-10-08 16:01:26 +0200

    Correct mount for log folder + add env variables for DQP

--- RunETLAIDA/runETL.sh
+++ RunETLAIDA/runETL.sh
@@ -20,9 +20,12 @@ curl -fsSL https://raw.githubusercontent.com/solventrix/Honeur-Setup/master/RunE
 touch etl-runner.env
 echo "THERAPEUTIC_AREA=honeur" >> etl-runner.env
 echo "REGISTRY=$REGISTRY" >> etl-runner.env
+echo "DATA_SET=AIDA" >> etl-runner.env
+echo "ORGANIZATION=AIDA" >> etl-runner.env
+echo "INDICATION=mm" >> etl-runner.env
 echo "LOG_LEVEL=INFO" >> etl-runner.env
 echo "LOG_FOLDER_HOST=$LOG_FOLDER_HOST" >> etl-runner.env
-echo "LOG_FOLDER=/var/log" >> etl-runner.env
+echo "LOG_FOLDER=/log" >> etl-runner.env
 echo "ETL_IMAGE_NAME=etl-aida/etl" >> etl-runner.env
 echo "ETL_IMAGE_TAG=current" >> etl-runner.env
 echo "DATA_FOLDER_HOST=$DATA_FOLDER_HOST" >> etl-runner.env
@@ -33,7 +36,9 @@ echo "CDM_SCHEMA=omopcdm_aida" >> etl-runner.env
 echo "VOCAB_SCHEMA=omopcdm_aida" >> etl-runner.env
 echo "RESULTS_SCHEMA=results_aida" >> etl-runner.env
 echo "DELIMITER=," >> etl-runner.env
-echo "RUN_DQD=no" >> etl-runner.env
+echo "RUN_DQD=yes" >> etl-runner.env
+echo "SCRIPT_UUID=30220b6a-a1c2-4e72-8ad3-f0873f53908b" >> etl-runner.env
+echo "SHARED_SCRIPT_VERSION_UUID=2661ee6e-c099-42a9-a5dd-47665c2b2014" >> etl-runner.env
 
 echo "Run ETL"
 docker run \
