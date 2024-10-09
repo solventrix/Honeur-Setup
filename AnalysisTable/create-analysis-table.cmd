8595950bd5af99f39715bc6210bbe8e23651cefb
Peter Moorthamer <moorthamer@gmail.com>
2024-10-09 11:14:06 +0200

    Add run script for Windows

---
+++ AnalysisTable/create-analysis-table.cmd
@@ -0,0 +1,11 @@
+@ECHO off
+
+SET REGISTRY=harbor.honeur.org
+SET REPOSITORY=distributed-analytics
+SET IMAGE=analysis-table-generator
+SET VERSION=1.1.10
+SET TAG=%VERSION%
+
+docker pull %REGISTRY%/%REPOSITORY%/%IMAGE%:%TAG%
+
+docker run --rm --name analysis-table-generator --env THERAPEUTIC_AREA=HONEUR --env VERSION=%VERSION% --env ANALYSIS_TABLE_SCHEMA=results --env ANALYSIS_TABLE_NAME=analysis_table --env ANALYSIS_TABLE_METADATA=analysis_table_metadata -v %CD%/results:/script/results --network feder8-net %REGISTRY%/%REPOSITORY%/%IMAGE%:%TAG%
