79a742992fa4a6d1278450645a266a06a85ac32a
Peter Moorthamer <moorthamer@gmail.com>
2025-01-29 14:10:29 +0100

    Release latest version of the Landmark study script (JIRA AEKR-2177)

--- LandmarkStudy/run-landmark-study.cmd
+++ LandmarkStudy/run-landmark-study.cmd
@@ -3,7 +3,7 @@
 SET REGISTRY=harbor.honeur.org
 SET REPOSITORY=study_36
 SET IMAGE=landmark-study
-SET VERSION=V5
+SET VERSION=V6
 SET TAG=%VERSION%
 
 echo "Docker login @ %REGISTRY%"
@@ -12,5 +12,5 @@ docker login %REGISTRY%
 echo "Pull Docker image"
 docker pull %REGISTRY%/%REPOSITORY%/%IMAGE%:%TAG%
 
-docker run --rm --name landmark-study --env THERAPEUTIC_AREA=HONEUR --env SCRIPT_UUID=eb4730b8-f4cb-4041-b5df-25f07014578a --network feder8-net %REGISTRY%/%REPOSITORY%/%IMAGE%:%TAG%
+docker run --rm --name landmark-study --env THERAPEUTIC_AREA=HONEUR --env SCRIPT_UUID=01b1a33f-52ca-4ed3-b5ef-f27666d2b218 -v "%CD%/results":/script/results  --network feder8-net %REGISTRY%/%REPOSITORY%/%IMAGE%:%TAG%
 
--- LandmarkStudy/run-landmark-study.sh
+++ LandmarkStudy/run-landmark-study.sh
@@ -4,7 +4,7 @@ set -ex
 REGISTRY=harbor.honeur.org
 REPOSITORY=study_36
 IMAGE=landmark-study
-VERSION=V5
+VERSION=V6
 TAG=$VERSION
 
 echo "Docker login @ $REGISTRY"
@@ -14,7 +14,8 @@ echo "Pull Docker image"
 docker pull $REGISTRY/$REPOSITORY/$IMAGE:$TAG
 
 docker run --rm --name landmark-study \
---env THERAPEUTIC_AREA=HONEUR --env SCRIPT_UUID=eb4730b8-f4cb-4041-b5df-25f07014578a \
+--env THERAPEUTIC_AREA=HONEUR --env SCRIPT_UUID=01b1a33f-52ca-4ed3-b5ef-f27666d2b218 \
+-v "$PWD/results":/script/results \
 --network feder8-net \
 $REGISTRY/$REPOSITORY/$IMAGE:$TAG
 
