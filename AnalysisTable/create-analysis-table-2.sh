925a28cb2fbbc39ac2ce21f50028e916ec5b1cf0
Peter Moorthamer <moorthamer@gmail.com>
2026-02-24 10:30:25 +0100

    Add alternative run script with new env variables

---
+++ AnalysisTable/create-analysis-table-2.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+set -ex
+
+REGISTRY=harbor.honeur.org
+REPOSITORY=distributed-analytics
+IMAGE=analysis-table-generator
+VERSION=latest
+TAG=$VERSION
+
+docker pull $REGISTRY/$REPOSITORY/$IMAGE:$TAG
+
+docker run --rm --name analysis-table-generator \
+--env THERAPEUTIC_AREA=HONEUR \
+--env ANALYSIS_TABLE_SCHEMA=results --env ANALYSIS_TABLE_NAME=analysis_table --env ANALYSIS_TABLE_METADATA=analysis_table_metadata \
+--env OPENBLAS_CORETYPE=Generic \
+--env CREATE_REPORTS=TRUE \
+-v ${PWD}/results:/script/results \
+--network feder8-net \
+$REGISTRY/$REPOSITORY/$IMAGE:$TAG
